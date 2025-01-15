079bde3737358cbec190d6fc5c5996562ebbf2f1
vegasleonidas <lhlewis@gmail.com>
2025-01-14 17:29:42 -0800

    First Commit

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+
+def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+
+main()
